e638089c85a231c40fe590ec5037ef0ca7d5274c
marguilherme <84818691+marguilherme@users.noreply.github.com>
2022-06-03 16:25:47 -0300

    feat: contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,21 @@
 export const ContactPage = () => (
     <>
     <div id="background-contact" className="container1">
-        <h1>
-            Contatos
-        </h1>
+        <h1>Entre em contato!</h1>
+        <form>
+            <label htmlFor="nome">Digite seu nome:</label>
+            <input type="text" id="nome" className="input-form" placeholder="nome"/>
+            <br/>
+
+            <label htmlFor="email">Digite seu email:</label>
+            <input type="text" id="email" className="input-form" placeholder="email@email.com"/>
+            <br/>
+
+            <label htmlFor="nome">Digite sua mensagem:</label>
+            <textarea id="nome" className="input-form" rows="5" />
+
+            <button type="button" className="enviar">Enviar</button>
+        </form>
     </div>
     </>
 );
