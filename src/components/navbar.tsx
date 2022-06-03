4805c11902fd6cb8b1d1ba0ad4fc198eed215750
marguilherme <84818691+marguilherme@users.noreply.github.com>
2022-06-03 14:54:09 -0300

    fix: fixed navbar home logo and backgrounds names

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ export const NavbarComponent = () => {
     <>
     <Navbar className="color-nav" expand="lg" sticky="top">
         <Container>
-            <Navbar.Brand href="/" ><div className="icone"/></Navbar.Brand>
+            <Link to="/" ><div className="icone"/></Link>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="dd-flex flex-row-reverse" id="basic-navbar-nav">
                 <Nav className="me-auto">
